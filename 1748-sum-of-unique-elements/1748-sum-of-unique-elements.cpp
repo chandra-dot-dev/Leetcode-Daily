0a8b9142fa539e99634545f8688a632952e0b9ae
Chandra Kanth Reddy <150149713+chandra-dot-dev@users.noreply.github.com>
2026-02-11 19:02:21 +0530

    Time: 0 ms (100%), Space: 10.6 MB (62.02%) - LeetHub

---
+++ 1748-sum-of-unique-elements/1748-sum-of-unique-elements.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int sumOfUnique(vector<int>& nums) {
+        unordered_map<int,int> mpp;
+        for(int num:nums){
+            mpp[num]++;
+        }
+        int ans =0;
+        for(auto it:mpp){
+            if(it.second==1){
+                ans+=it.first;
+            }
+        }
+        return ans;
+    }
+};
