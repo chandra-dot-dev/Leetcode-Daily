d3c7068debe8f507ebd7dff56fd2e06cd4bd9ac5
Chandra Kanth Reddy <150149713+chandra-dot-dev@users.noreply.github.com>
2026-03-20 18:03:34 +0530

    Time: 0 ms (100%), Space: 26.7 MB (5.46%) - LeetHub

---
+++ 3289-the-two-sneaky-numbers-of-digitville/3289-the-two-sneaky-numbers-of-digitville.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> getSneakyNumbers(vector<int>& nums) {
+        unordered_map<int,int> mpp;
+        set<int> st;
+        vector<int> ans;
+        for(int num:nums){
+            mpp[num]++;
+        }
+        for(int num:nums){
+            if(mpp[num] == 2){
+                st.insert(num);
+            }
+        }
+        for(int num:st){
+            ans.push_back(num);
+        }
+        return ans;
+        
+    }
+};
