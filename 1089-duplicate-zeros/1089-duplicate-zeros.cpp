4ff9dba3c36f2805234eb797c5b208d0de751026
Chandra Kanth Reddy <150149713+chandra-dot-dev@users.noreply.github.com>
2026-03-26 21:08:44 +0530

    Time: 0 ms (100%), Space: 13.9 MB (67.03%) - LeetHub

---
+++ 1089-duplicate-zeros/1089-duplicate-zeros.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    void duplicateZeros(vector<int>& arr) {
+        int i = 0;
+        while (i<arr.size()) {
+
+            if (arr[i] == 0) {
+                arr.insert(arr.begin() + i + 1, 0);
+                arr.pop_back();
+                i+=1;
+            }
+            i+=1;
+        }
+    }
+};
