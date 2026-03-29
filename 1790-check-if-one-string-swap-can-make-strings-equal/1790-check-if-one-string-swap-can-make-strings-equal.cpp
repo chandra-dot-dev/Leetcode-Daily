50901229333a15d563d59b0e4ce272be4613fe8d
Chandra Kanth Reddy <150149713+chandra-dot-dev@users.noreply.github.com>
2026-03-29 17:11:52 +0530

    Time: 0 ms (100%), Space: 8.4 MB (36.58%) - LeetHub

--- 1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.cpp
+++ 1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.cpp
@@ -3,14 +3,20 @@ public:
     bool areAlmostEqual(string s1, string s2) {
         if(s1.size()!=s2.size()) return false;
         if(s1==s2) return true;
-        vector<int> diff;
+        vector<char> diff;
         for(int i =0;i<s1.size();i++){
             if(s1[i]!=s2[i]){
                 diff.push_back(i);
             }
-
         }
-        if(diff.size()!=2) return false;
-        return (s1[diff[0]] == s2[diff[1]] && s2[diff[0]]==s1[diff[1]]);
+        if(diff.size()==0){
+            return true;
+        }else if(diff.size()!=2){
+            return false;
+        }else{
+            swap(s1[diff[0]],s1[diff[1]]);
+        }
+        return s1==s2;
+
     }
 };
